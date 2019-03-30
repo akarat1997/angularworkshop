5e5c4a620816b405ed26c2eb06c5e86ec41b734d
akarat.p <akarat.p@hotmail.com>
2019-03-30 14:07:47 +0700

    update

--- gitrepo/src/app/app.component.ts
+++ gitrepo/src/app/app.component.ts
@@ -6,50 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gitrepo';
-  score: number = 49;
-  name: string = "นาย ก";
-
-  student:any = {
-    name: "Akarat",
-    studentId: "5921600237",
-    weight: 60,
-    height: 168
-  }
-
-  studentlist = [{
-    name: "Lose",
-    studentId: "5921600333",
-    weight: 60,
-    height: 168
+  productList = [{
+    pId: "0001",
+    pname: "แปรงสีฟัน",
+    pCost: 200
   },
   {
-    name: "Jeney",
-    studentId: "5921600222",
-    weight: 58,
-    height: 158
+    pId: "0002",
+    pname: "แก้วน้ำ",
+    pCost: 10
   },
   {
-    name: "Lisa",
-    studentId: "5921600111",
-    weight: 42,
-    height: 170
-  },
-  {
-    name: "Jisoo",
-    studentId: "5921600177",
-    weight: 44,
-    height: 178
-  }
-  ];
+    pId: "0003",
+    pname: "ยาสีฟัน",
+    pCost: 20
+  }]
 
   constructor() {
-    this.student.bmi = (this.student.weight / ((this.student.height / 100) * (this.student.height / 100))).toFixed(2);
-    this.studentlist.map((object, index) => {
-      let obj: any = object;
-      obj.bmi = (object.weight / ((object.height / 100) * (object.height / 100))).toFixed(2);
-      return obj;
-    })
   }
 
 }
