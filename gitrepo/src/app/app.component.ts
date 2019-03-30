d6e40433ec494a9e63be582ff34e6d2af0a87464
akarat.p <akarat.p@hotmail.com>
2019-03-30 15:47:06 +0700

    ws2

--- gitrepo/src/app/app.component.ts
+++ gitrepo/src/app/app.component.ts
@@ -6,21 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  productList = [{
-    pId: "0001",
-    pname: "แปรงสีฟัน",
-    pCost: 200
+ 
+ productList = [{
+    pId : "0001",
+    pname : "ยาสีฟัน",
+    pCost : 50
   },
   {
-    pId: "0002",
-    pname: "แก้วน้ำ",
-    pCost: 10
+    pId : "0002",
+    pname : "แก้วน้ำ",
+    pCost : 50
   },
   {
-    pId: "0003",
-    pname: "ยาสีฟัน",
-    pCost: 20
-  }]
+    pId : "0003",
+    pname : "แปรงสีฟัน",
+    pCost : 50
+  }
+  ];
+ selectedProduct :any;
+
+  selectProduct(p){
+    this.selectedProduct = p;
+  }
 
   constructor() {
   }
